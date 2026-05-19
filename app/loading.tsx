82cd4952ebbd884c2370389ca7a19929886f3ab8
lastleo4u <sadeekpro2001@gmail.com>
2026-05-19 20:22:16 +0100

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,18 @@
+export default function Loading() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "var(--wave-bg)",
+        color: "var(--wave-blue)",
+        fontSize: "2rem",
+        fontWeight: "bold",
+      }}
+    >
+      Loading Wave...
+    </div>
+  );
+}
